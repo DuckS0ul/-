a3a3a34b3afe03656606797602820fef1d7a89fd
Junnan Li <117548040+DuckS0ul@users.noreply.github.com>
2024-09-25 14:39:45 +0800

    Add files via upload

---
+++ 有效括号/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace 有效括号
+{
+
+    internal class Program
+    {
+        public static bool IsValid(string s)
+        {
+            // 如果字符串长度是奇数，不可能是有效的括号
+            if (s.Length % 2 != 0)
+            {
+                return false;
+            }
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    char top = stack.Pop();
+
+                    if ((c == ')' && top != '(') ||
+                        (c == '}' && top != '{') ||
+                        (c == ']' && top != '['))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            string input = "()[]{}";  // 你可以改变这个输入来测试不同的括号组合
+            bool result = IsValid(input);
+            Console.WriteLine($"输入: {input}, 括号是否有效: {result}");
+        }
+    }
+}
---
+++ 移动零/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace 移动零
+{
+    public class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int slow = 0;
+
+            for (int fast = 0; fast < nums.Length; fast++)
+            {
+                if (nums[fast] != 0)
+                {
+                    nums[slow] = nums[fast];
+                    slow++;
+                }
+            }
+
+            for (int i = slow; i < nums.Length; i++)
+            {
+                nums[i] = 0;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 1, 0, 3, 12 };
+            Solution solution = new Solution();
+            solution.MoveZeroes(nums);
+
+            Console.WriteLine("移动零之后的数组:");
+            foreach (int num in nums)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
