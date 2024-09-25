a171c9658731b87f49bf06e6687af3867487af67
Junnan Li <117548040+DuckS0ul@users.noreply.github.com>
2024-09-25 14:35:47 +0800

    Add files via upload

---
+++ 每日温度/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace 每日温度
+{
+    internal class Program
+    {
+        public static int[] GetDailyTemperatures(int[] temperatures)
+        {
+            int n = temperatures.Length;
+            int[] result = new int[n];
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                while (stack.Count > 0 && temperatures[i] > temperatures[stack.Peek()])
+                {
+                    int prevDay = stack.Pop();
+                    result[prevDay] = i - prevDay;
+                }
+                stack.Push(i);
+            }
+
+            while (stack.Count > 0)
+            {
+                result[stack.Pop()] = 0;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] temperatures = { 73, 74, 75, 71, 69, 72, 76, 73 };
+            int[] result = GetDailyTemperatures(temperatures);
+
+            Console.WriteLine("每日温度结果: ");
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
---
+++ 颜色分类/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace 颜色分类
+{
+    public class Solution
+    {
+        public void SortColors(int[] nums)
+        {
+            int low = 0;
+            int current = 0;
+            int high = nums.Length - 1;
+
+            while (current <= high)
+            {
+                if (nums[current] == 0)
+                {
+                    Swap(nums, current, low);
+                    low++;
+                    current++;
+                }
+                else if (nums[current] == 2)
+                {
+                    Swap(nums, current, high);
+                    high--;
+                }
+                else
+                {
+                    current++;
+                }
+            }
+        }
+
+        private void Swap(int[] nums, int i, int j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
