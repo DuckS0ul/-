f8a33e6fac07917fe4e7df057854fa12da7f7ccc
Junnan Li <117548040+DuckS0ul@users.noreply.github.com>
2024-09-25 14:17:42 +0800

    Add files via upload

---
+++ 根到叶子节点的数字之和/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace 根到叶子节点的数字之和
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public int SumNumbers(TreeNode root, int currentSum = 0)
+        {
+            if (root == null) return 0;
+
+            currentSum = currentSum * 10 + root.val;
+
+            if (root.left == null && root.right == null)
+            {
+                return currentSum;
+            }
+
+            return SumNumbers(root.left, currentSum) + SumNumbers(root.right, currentSum);
+        }
+
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 构造一个二叉树
+            TreeNode root = new TreeNode(4);
+            root.left = new TreeNode(9);
+            root.right = new TreeNode(0);
+            root.left.left = new TreeNode(5);
+            root.left.right = new TreeNode(1);
+
+            Solution solution = new Solution();
+            int result = solution.SumNumbers(root);
+
+            Console.WriteLine($"根到叶子节点的数字之和是: {result}");
+
+        }
+    }
+}
---
+++ 轮转数组/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace 轮转数组
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int k = 3;
+
+
+        }
+
+        static void RotateArray(int[] array, int k)
+        {
+            int length = array.Length;
+            k = k % length; // 确保轮转次数不超过数组长度
+
+            Reverse(array, 0, length - 1);
+            Reverse(array, 0, k - 1);
+            Reverse(array, k, length - 1);
+        }
+
+        static void Reverse(int[] array, int start, int end)
+        {
+            while (start < end)
+            {
+                int temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
